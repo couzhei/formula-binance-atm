baebc95317064ce58eee9ee7a15ab8449044debe
couzhei <couzhei@gmail.com>
2025-02-03 06:14:48 +0000

    - Stable version so far

--- frontend/my-app/next.config.ts
+++ frontend/my-app/next.config.ts
@@ -22,8 +22,12 @@ module.exports = {
   },
   devServer: {
     https: {
-      key: fileSync.readFileSync(address.join(__dirname, 'localhost.key')),
-      cert: fileSync.readFileSync(address.join(__dirname, 'localhost.crt')),
+      // key: fileSync.readFileSync(address.join(__dirname, 'localhost.key')),
+      // cert: fileSync.readFileSync(address.join(__dirname, 'localhost.crt')),
+      key: fileSync.readFileSync(address.join(__dirname, 'key.pem')),
+      cert: fileSync.readFileSync(address.join(__dirname, 'cert.pem')),
+      // key: fileSync.readFileSync(address.join(__dirname, 'PRIVATE.key')),
+      // cert: fileSync.readFileSync(address.join(__dirname, 'PUBLIC.pem')),
     },
   },
 };
