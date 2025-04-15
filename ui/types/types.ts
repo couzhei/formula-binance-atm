46eb93abb741de720f384bbf1111734b08f9713a
couzhei <couzhei@gmail.com>
2025-04-15 20:10:20 +0330

    SoB was in the `.gitignore`

---
+++ ui/types/types.ts
@@ -0,0 +1,32 @@
+export interface HistoricalData {
+  timestamp: number;
+  open: number;
+  high: number;
+  low: number;
+  close: number;
+  volume: number;
+  [key: string]: number | string | undefined;
+}
+
+export interface Signal {
+  timestamp: number;
+  price: number;
+  type: "BUY" | "SELL";
+}
+
+export interface ApiResponse {
+  historical_data: HistoricalData[];
+  signals: Signal[];
+  sma_param: number;
+}
+
+export interface RealTimeData {
+  time: number;
+  open: number;
+  high: number;
+  low: number;
+  close: number;
+  sma: number | null;
+  signal?: "BUY" | "SELL" | null;
+  is_final?: boolean;
+}
